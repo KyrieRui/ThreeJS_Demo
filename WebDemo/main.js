1f0d6ed6d20de9a5c388b67b332bcd8c35eb62ef
KyrieRui <kyrie19960324@gmail.com>
2023-10-09 04:13:26 +1300

    ‘WebDemo’ add texture to my 3D project

--- WebDemo/main.js
+++ WebDemo/main.js
@@ -1,36 +1,47 @@
 import * as THREE from "three";
 
-// Scene
+const canvas = document.getElementById("webgl");
 const scene = new THREE.Scene();
 
-// Create sphere
-// geometry is like clay, it's the shape
-const geometry = new THREE.SphereGeometry(3, 64, 64); // this premiters are radius, widthSegments, heightSegments
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
+camera.position.z = 5;
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-  color: "#00ff83", // this is the color
-}); // this is the color
+  // color: 0xff0000,
+  map: new THREE.TextureLoader().load("randaling_t-_rex_animated/textures/material_0_diffuse.png"),
+  shininess: 200,
+});
 
-const mesh = new THREE.Mesh(geometry, material); // combine the geometry and the material
-scene.add(mesh); // add the mesh to the scene
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
 
-// Light
-const light = new THREE.PointLight(0xffffff, 1, 100); // this premiters are color, intensity
-light.position.set(0, 10, 10); // this is the position of the light
-scene.add(light); // add the light to the scene
-
-// Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600); // this premiters are fov, aspect, near, far
-camera.position.z = 20; // this is the position of the camera
-scene.add(camera); // add the camera to the scene
-
-// light helper
-// const lightHelper = new THREE.PointLightHelper(0xffffff);
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(gridHelper);
-
-// Renderer
-const canvas = document.querySelector("#webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600); // this premiters are width, height
-renderer.render(scene, camera); // this premiters are scene, camera
+renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+const tick = () => {
+  renderer.render(scene, camera);
+
+  box.rotation.x += 0.01;
+  box.rotation.y += 0.01;
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
